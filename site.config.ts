a024d9ca43705dd56ed57e6ffd6d90e3ff9cde30
zyuxi <92156792+zyuxi@users.noreply.github.com>
2022-06-21 18:30:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'XXX',
+  github: 'XXX',
+  linkedin: 'XXX',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
